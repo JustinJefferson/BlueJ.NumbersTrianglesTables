45d9b1f1bae07f3d44278a646ffa7ca883728eaf
Justin <Jefferson@zipcoders-MacBook-Pro-10.local>
2019-05-23 15:27:38 -0400

    Finished first 2 parts

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,62 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        String result = "";
+        for(int i = 0; i < stop; i++){
+            result = result + i;
+        }
+        return result;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String result = "";
+        for(int i = start; i < stop; i++){
+            result = result + i;
+        }
+        return result;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result = "";
+        int increment = start;
+        while(increment < stop){
+            result = result + increment;
+            increment += step;
+        }
+        return result;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for(int i = start; i < stop; i++){
+            if(i % 2 == 0) result += i;
+        }
+        return result;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for(int i = start; i < stop; i++){
+            if(i % 2 != 0) result += i;
+        }
+        return result;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String result = "";
+        int num = 0;
+        int finalNum = 0;
+        for(int i = start; i <= stop; i++){
+            num = i;
+            finalNum = i;
+            for(int j = 1; j < exponent; j++){
+                finalNum *= num;
+            }
+            result += finalNum;
+        }
+        return result;
     }
 }
